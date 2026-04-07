52ff8470072c3b4a7de491a121fcc1071446d89f
dan-kagi <65138417+dan-kagi@users.noreply.github.com>
2026-04-06 22:02:29 -0300

    clear button

--- theoretical_recovery/index.js
+++ theoretical_recovery/index.js
@@ -70,8 +70,9 @@ function clear() {
   }
 }
 const clear_button = document.getElementById('clear');
-clear.addEventListener('click', () => {
+clear_button.addEventListener('click', () => {
   clear();
 });
+
 document.querySelector('footer').innerHTML =
   `<p>Copyright &copy; Danilo Morais Itokagi. All rights reserved. ${new Date().getFullYear()}</p>`;
